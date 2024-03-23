db80aad2973461543f5f1463a34bf994c33ce69a
Gautam <jha.gautam306@gmail.com>
2024-03-24 03:02:55 +0530

    read json file

--- frontend/my-app/src/Component/AddDomain.js
+++ frontend/my-app/src/Component/AddDomain.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from "../Component/Navbar"
 import email_icon from '../Assets/email.png'
 import domain_icon from '../Assets/domain_icon.png'
@@ -7,6 +7,34 @@ import url_icon from '../Assets/www_icon.png'
 
 const AddDomain = () => {
     document.title='Add Domain';
+    
+    const [file, setFile] = useState(null);
+    let fileReader;
+    const handleFileRead = async(e) => {
+            const content = fileReader.result;
+
+
+            console.log(typeof(content));
+            const dnsRecordsData = JSON.parse(content); 
+            console.log(dnsRecordsData); 
+                
+                
+            for (const key in dnsRecordsData) {
+                
+            console.log(dnsRecordsData[key] );
+
+            }
+    }
+
+      const handleFileChosen = (file) => {
+        
+        fileReader = new FileReader();
+        
+        fileReader.onloadend = handleFileRead;
+        
+        fileReader.readAsText(file);
+        
+      };
   return (
     <div>
       <Navbar/>
@@ -17,7 +45,7 @@ const AddDomain = () => {
         
         <form onSubmit={onsubmit} class="home_file">
         
-            <input type="file" class="home_file_input" name="uploadedimage"  aria-describedby="inputGroupFileAddon03" aria-label="Upload" accept=".json, .cvs"/>
+            <input type="file" class="home_file_input" name="uploadedimage" onChange={e => handleFileChosen(e.target.files[0])}  aria-describedby="inputGroupFileAddon03" aria-label="Upload" accept=".json, .cvs"/>
             <button type="submit" id="cbtn" class="home_addbtn" >Submit</button>
             
         </form>
